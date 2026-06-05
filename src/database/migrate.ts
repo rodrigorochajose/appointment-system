289e584df7d26438839c743855ef429b4c9df630
Rodrigo Rocha <rodrigorochajose@hotmail.com>
2026-06-04 22:07:46 -0300

    fix: update mysql import syntax for consistency

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 import { migrate } from 'drizzle-orm/mysql2/migrator';
-import mysql from 'mysql2/promise';
+import * as mysql from 'mysql2/promise';
 import * as dotenv from 'dotenv';
 import { log } from '../common/logger';
 import { getDbCredentials } from './db-credentials';
